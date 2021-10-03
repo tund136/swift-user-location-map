112e7690fe9715c9a36bb446d03123ed3de221d0
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 04:07:34 +0700

    Create the project

---
+++ UserLocationOnMap/UserLocationOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationOnMapApp.swift
+//  UserLocationOnMap
+//
+//  Created by Danh Tu on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
