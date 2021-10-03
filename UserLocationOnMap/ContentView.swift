24acf14673cebcfd9d276a31c6b463d36dbc0412
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 04:19:29 +0700

    Begin User Location Process

--- UserLocationOnMap/ContentView.swift
+++ UserLocationOnMap/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     )
     
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, showsUserLocation: true)
             .ignoresSafeArea()
     }
 }
